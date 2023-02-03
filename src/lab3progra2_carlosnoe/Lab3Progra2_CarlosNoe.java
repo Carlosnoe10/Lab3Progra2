bb297a61212e6abf008a7a40a3db078fd8763455
Carlosnoe10 <carlosdnoem@gmail.com>
2023-02-03 11:19:49 -0800

    Terminamo el menu y el plan :3

--- src/lab3progra2_carlosnoe/Lab3Progra2_CarlosNoe.java
+++ src/lab3progra2_carlosnoe/Lab3Progra2_CarlosNoe.java
@@ -1,20 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab3progra2_carlosnoe;
 
-/**
- *
- * @author cd507
- */
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Lab3Progra2_CarlosNoe {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner k1ng = new Scanner(System.in);
+
+        System.out.println("Bienvenid al menu \n"
+                + "1. CRUD Concecionaria \n"
+                + "2. CRUD Clientes \n"
+                + "3. CRUD Vehiculos \n"
+                + "4. Compra y venta de vehiculos");
+        int menu = k1ng.nextInt();
+        for (int x = 0; x < 1; x++) {
+            while (menu != 4) {
+                if ((menu > 0) && (menu < 4)) { // 4 es igual al numero limite que deseo agregar
+                    x++;
+                    switch (menu) {
+                        case 1:
+                                break;
+
+                    }// switch menu p
+                System.out.println("Bienvenid al menu \n"
+                + "1. CRUD Concecionaria \n"
+                + "2. CRUD Clientes \n"
+                + "3. CRUD Vehiculos \n"
+                + "4. Compra y venta de vehiculos");
+                menu = k1ng.nextInt();
+                
+                } else {
+                    System.out.println("Ingrese adecuadamente el numero");
+                    x--;
+                }//if
+            }//while
+        }//for
+
+        System.out.println("Fin");
     }
-    
 }
